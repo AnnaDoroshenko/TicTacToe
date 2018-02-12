84adb6fd76772a6ed0ff9849b8be95956d9c545e
AnnaDoroshenko <annadoroshenko03@gmail.com>
2018-02-13 00:38:53 +0200

    some additions

--- main.cpp
+++ main.cpp
@@ -5,11 +5,11 @@ struct Grid
     static const unsigned int gridSize = 3;
     static const char tokenX = 'x';
     static const char tokenO = 'o';
-    static char gridArray [gridSize][gridSize];
+    static const char defaultToken = '.';
+    char gridArray [gridSize][gridSize];
 
     Grid()
     {
-        setDefaultGrid();
     }
 
     void setDefaultGrid()
@@ -18,19 +18,20 @@ struct Grid
         {
             for (unsigned int j = 0; j < gridSize; j++)
             {
-                gridArray[i][j] = '.';
+                gridArray[i][j] = defaultToken;
             }
         }
     }
 
-    void showStatus()
+    void showGridStatus()
     {
         for (unsigned int i = 0; i < gridSize; i++)
         {
             for (unsigned int j = 0; j < gridSize; j++)
             {
-                std::cout << "[" << gridArray[i][j] << "]" << std::endl;
+                std::cout << "[" << gridArray[i][j] << "]";
             }
+            std::cout << "" << std::endl;
         }
     }
 
@@ -40,35 +41,56 @@ struct Grid
         {
             for (unsigned int j = 0; j < gridSize; j++)
             {
-                if ((gridArray[i][j] != tokenX) || (gridArray[i][j] != tokenO))
+                if (gridArray[i][j] == defaultToken)
                 {
+                    return false;
                     break;
                 }
                 else 
                 {
-                    return true;
+                    continue;
                 }
             }
         }
-        return false;
+        return true;
     }
 };
 
 struct Player
 {
-    static char token;
+    char token;
 
     Player()
     {
     }
 
-    void setTokens(char token)
+    void setToken(char token)
     {
         this->token = token;
+        std::cout << "For checking " << this->token << std::endl;
+    }
+
+    void setPositionOfToken(unsigned int x, unsigned int y)
+    {
+        grid[x][y] = token; // Look about references!!!
     }
 };
 
 int main()
 {
+    unsigned int x;
+    unsigned int y;
 
+    Grid testGrid;
+    testGrid.setDefaultGrid();
+    testGrid.showGridStatus();
+    // std::cout << testGrid.isFullGrid() << std::endl;
+
+    Player testPlayer;
+    testPlayer.setToken('x');
+    std::cout << "Check input" << std::endl;
+    std::cin >> x;
+    std::cin >> y;
+    testPlayer.setPositionOfToken(x, y);
+    testGrid.showGridStatus();
 }
