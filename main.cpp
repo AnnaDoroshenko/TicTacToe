6d8189d612031f3d9d557dca4369f25c22d6b243
AnnaDoroshenko <annadoroshenko03@gmail.com>
2018-02-15 23:12:31 +0200

    something workable

--- main.cpp
+++ main.cpp
@@ -3,8 +3,8 @@
 struct Grid
 {
     static const unsigned int gridSize = 3;
-    static const char tokenX = 'x';
-    static const char tokenO = 'o';
+    // static const char tokenX = 'x';
+    // static const char tokenO = 'o';
     static const char defaultToken = '.';
     char gridArray [gridSize][gridSize];
 
@@ -21,6 +21,11 @@ struct Grid
                 gridArray[i][j] = defaultToken;
             }
         }
+    }
+
+    char setTokenIntoGrid(unsigned int x, unsigned int y, char token)
+    {
+        gridArray[x][y] = token;
     }
 
     void showGridStatus()
@@ -60,37 +65,42 @@ struct Player
 {
     char token;
 
-    Player()
-    {
-    }
-
-    void setToken(char token)
+    Player(char token)
     {
         this->token = token;
-        std::cout << "For checking " << this->token << std::endl;
     }
 
-    void setPositionOfToken(unsigned int x, unsigned int y)
+    char getToken()
     {
-        grid[x][y] = token; // Look about references!!!
+        return token;
     }
 };
 
 int main()
 {
+    const char tokenX = 'x';
+    const char tokenO = 'o';
+    
     unsigned int x;
     unsigned int y;
 
     Grid testGrid;
     testGrid.setDefaultGrid();
     testGrid.showGridStatus();
-    // std::cout << testGrid.isFullGrid() << std::endl;
-
-    Player testPlayer;
-    testPlayer.setToken('x');
-    std::cout << "Check input" << std::endl;
+    
+    Player testPlayer1(tokenX);
+    Player testPlayer2(tokenO);
+    const char firstPlayerToken = testPlayer1.getToken();
+    const char secondPlayerToken = testPlayer2.getToken();
+    std::cout << "First player input: " << std::endl;
     std::cin >> x;
     std::cin >> y;
-    testPlayer.setPositionOfToken(x, y);
+    testGrid.setTokenIntoGrid(x, y, firstPlayerToken);
     testGrid.showGridStatus();
+    std::cout << "Second player input: " << std::endl;
+    std::cin >> x;
+    std::cin >> y;
+    testGrid.setTokenIntoGrid(x, y, secondPlayerToken);
+    testGrid.showGridStatus();
+    // std::cout << testGrid.isFullGrid() << std::endl;
 }
